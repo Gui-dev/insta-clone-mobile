5fe0cd59e3e8c53798060bb98982cb4af8be924d
Gui-dev <adrianon10@gmail.com>
2021-10-17 18:00:53 -0300

    feat: add config routes

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+
+import { Routes } from './src/routes'
 
 export default function App () {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <>
+      <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
+    </>
   )
 }
