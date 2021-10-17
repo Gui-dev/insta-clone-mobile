a3b76b8698a5f648cbb13e77551e3051fabd3682
Gui-dev <adrianon10@gmail.com>
2021-10-17 20:51:14 -0300

    feat: add google fonts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import { View } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import AppLoading from 'expo-app-loading'
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto'
 
 import { Routes } from './src/routes'
 
 export default function App () {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
+  if (!fontsLoaded) {
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <AppLoading />
+    </View>
+  }
+
   return (
     <>
       <NavigationContainer>
