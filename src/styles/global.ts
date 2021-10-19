b5a780a736bca43d131c9416370812027f170ced
Gui-dev <adrianon10@gmail.com>
2021-10-19 01:22:23 -0300

    style: add global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,17 @@
+export const fonts = {
+  heading: '20px',
+  normal: '16px',
+  family: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold'
+  }
+}
+
+export const colors = {
+  'gray-900': '#0A0A0D',
+  'gray-800': '#222',
+  'gray-500': '#707C8C',
+  'gray-400': '#A8B2BF',
+  'purple-600': '#5639BF',
+  'white-300': '#e2e2e2'
+}
