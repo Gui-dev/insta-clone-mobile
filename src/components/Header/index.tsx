fc68b250a01d6c5c401981bede587409d8173a15
Gui-dev <adrianon10@gmail.com>
2021-10-29 04:12:26 -0300

    feat: add info user

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,37 @@
 import React from 'react'
+// @ts-ignore
+import { Gravatar } from 'react-native-gravatar'
 import { Feather } from '@expo/vector-icons'
 
-import { Container, Content, Title } from './style'
+import { useAuth } from './../../hooks/useAuth'
+
+import { Container, Content, Brand, Title, InfoUser, UserName } from './style'
 
 export const Header = () => {
+  const { user } = useAuth()
+
   return (
     <Container>
       <Content>
-        <Feather name="instagram" size={32} color="#e2e2e2"/>
-        <Title>MyInsta</Title>
+        <Brand>
+          <Feather name="instagram" size={32} color="#e2e2e2"/>
+          <Title>MyInsta</Title>
+        </Brand>
+        { !!user && (
+          <InfoUser>
+            <UserName>{ user.name }</UserName>
+            <Gravatar options={{
+              email: user.email,
+              secure: true
+            }}
+              style={{
+                height: 30,
+                width: 30,
+                borderRadius: 15
+              }}
+            />
+          </InfoUser>
+        ) }
       </Content>
     </Container>
   )
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -5,13 +5,20 @@ import { colors, fonts } from './../../styles/global'
 
 export const Container = styled.View`
   margin-top: ${Platform.OS === 'ios' ? '20px' : '0'};
-  padding: 20px;
+  padding: 20px 0;
   background-color: ${colors['gray-900']};
   border-bottom-width: 1px;
   border-color: ${colors['gray-800']};
 `
 
 export const Content = styled.View`
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  margin: 0 20px;
+`
+
+export const Brand = styled.View`
   align-items: center;
   flex-direction: row;
 `
@@ -21,4 +28,16 @@ export const Title = styled.Text`
   font-family: ${fonts.family.bold};
   color: ${colors['white-300']};
   margin-left: 10px;
+`
+
+export const InfoUser = styled.View`
+  align-items: center;
+  flex-direction: row;
+`
+
+export const UserName = styled.Text`
+  font-size: ${fonts.normal};
+  font-family: ${fonts.family.bold};
+  color: ${colors['white-300']};
+  margin-right: 5px;
 `
