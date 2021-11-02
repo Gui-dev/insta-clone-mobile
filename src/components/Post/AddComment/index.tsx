b7cf73e1b83b60e01e3be35ef2d1fd473b95b951
Gui-dev <adrianon10@gmail.com>
2021-11-02 04:08:09 -0300

    feat: add helper function handleAddComment

--- src/components/Post/AddComment/index.tsx
+++ src/components/Post/AddComment/index.tsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Feather } from '@expo/vector-icons'
 
 import { Container, Content, Input, ButtonSend } from './style'
 
-export const AddComment = () => {
+type AddCommentProps = {
+  postId: string
+  onComment: (postId: string, comment: string) => Promise<void>
+}
+
+export const AddComment = ({ postId, onComment }: AddCommentProps) => {
+  const [comment, setComment] = useState('')
+
+  const handleAddComment = async () => {
+    await onComment(postId, comment)
+    setComment('')
+  }
+
   return (
     <Container>
       <Content>
         <Input
           placeholder="Pode comentar..."
+          value={ comment }
+          onChangeText={ setComment }
         />
-        <ButtonSend>
+        <ButtonSend onPress={ handleAddComment }>
           <Feather name="send" size={24} color="#FFF"/>
         </ButtonSend>
       </Content>
