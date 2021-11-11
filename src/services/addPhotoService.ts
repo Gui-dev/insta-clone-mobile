2ca53bd21766941a7a58f1e0787e3fb24adfd724
Gui-dev <adrianon10@gmail.com>
2021-11-10 22:19:24 -0300

    feat: add addPhotoService, service to add a photo to firabase storage

---
+++ src/services/addPhotoService.ts
@@ -0,0 +1,31 @@
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage'
+import { Platform } from 'react-native'
+import uuid from 'react-native-uuid'
+import { getPictureBlob } from '../utils/getPictureBlob'
+
+import { firebaseStorage } from './../services/firebase'
+
+type PhotoResponse = {
+  id: string
+  url: string
+}
+
+export const addPhotoService = async (imageUri: string | undefined): Promise<PhotoResponse> => {
+  if (imageUri) {
+    const imageName = uuid.v4()
+
+    const uploadBlob = await getPictureBlob(Platform.OS === 'ios' ? imageUri?.replace('file://', '') : imageUri)
+    const newFile = ref(firebaseStorage, `images/${imageName}`)
+    const upload = await uploadBytes(newFile, uploadBlob as Blob)
+    const photoUrl = await getDownloadURL(upload.ref)
+
+    console.log('PHOTO: ', photoUrl)
+
+    return {
+      id: upload.ref.name,
+      url: photoUrl
+    }
+  } else {
+    throw new Error('Tipo de arquivo não permitido')
+  }
+}
