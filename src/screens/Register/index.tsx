d9b28ef9abb93adae400509f19dd77e627137ac4
Gui-dev <adrianon10@gmail.com>
2021-11-18 02:13:22 -0300

    feat: add new field

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -8,17 +8,26 @@ import {
   Container, Form, Input, ButtonRegister, ButtonRegisterText,
   ButtonLogin, ButtonLoginText
 } from './style'
+import { addUserService } from '../../services/addUserService'
 
 type NavigationProps = NativeStackNavigationProp<RootStackParamList, 'Login'>
 
 export const Register: React.FC = () => {
   const { navigate } = useNavigation<NavigationProps>()
   const [name, setName] = useState('')
+  const [nickname, setNickname] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  const handleRegister = () => {
-    console.log('Logado')
+  const handleRegister = async () => {
+    const data = {
+      name,
+      nickname,
+      email,
+      password
+    }
+    await addUserService(data)
+    navigate('Login')
   }
 
   const handleNavigationToLogin = () => {
@@ -31,12 +40,22 @@ export const Register: React.FC = () => {
         <Input
           placeholder="Nome"
           autoFocus
+          autoCapitalize="words"
           value={ name }
           onChangeText={ setName }
         />
         <Input
+          placeholder="Nickname"
+          autoFocus
+          autoCapitalize="none"
+          value={ nickname }
+          onChangeText={ setNickname }
+        />
+        <Input
           placeholder="E-mail"
           autoFocus
+          autoCapitalize="none"
+          autoCorrect={ false }
           keyboardType="email-address"
           value={ email }
           onChangeText={ setEmail }
