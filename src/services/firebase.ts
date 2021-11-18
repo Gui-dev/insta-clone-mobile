5a9fa2843cea47b794e84e514cbebe1d4f7b0de1
Gui-dev <adrianon10@gmail.com>
2021-11-18 02:04:15 -0300

    feat: add config firebase auth

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,15 +1,18 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
 import { getStorage } from 'firebase/storage'
+import { getAuth } from 'firebase/auth'
 
 import { firebaseConfig } from './../config/firebase'
 
 const firebaseApp = initializeApp(firebaseConfig)
 const database = getDatabase(firebaseApp)
 const firebaseStorage = getStorage(firebaseApp)
+const firebaseAuth = getAuth(firebaseApp)
 
 export {
   firebaseApp,
   database,
-  firebaseStorage
+  firebaseStorage,
+  firebaseAuth
 }
